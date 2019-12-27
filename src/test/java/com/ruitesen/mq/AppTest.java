c5bdce14198384363d35b2cdaa00b8106d63b5a2
as.yang <yanganshihf@163.com>
2019-12-27 16:51:43 +0800

    更新注释

--- src/test/java/com/ruitesen/mq/AppTest.java
+++ src/test/java/com/ruitesen/mq/AppTest.java
@@ -9,7 +9,6 @@ import com.ruitesen.mq.test.Testcustomer;
  * 测试单元
  * 
  * @author yanganshi
- *
  */
 public class AppTest {
 	String[] args;
@@ -17,8 +16,10 @@ public class AppTest {
 	@Test
  	public void test() throws Exception {
 		System.out.println("启用测试单元...");
+		// 建立两个消息接收者
 		Testcustomer.main(args);
 		Testcustomer.main(args);
+		// 生产消息
 		TestProducer.main(args);
 	}
 }
